db1ce418931fc74188f5b5506902924d6370b15a
Adam Scoggins <scoggins.aj@gmail.com>
2022-08-16 22:23:50 -0500

    Finished

--- phase-2.js
+++ phase-2.js
@@ -2,14 +2,26 @@ const [addNums, addManyNums] = require("./phase-1");
 
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
-  // Fill this in
+    let arr = [];
 
+    for (let i = increment; i <= increment * 10; i += increment) {
+        const res = addNums(i);
+        arr.push(res);
+    }
+
+    return arr;
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
-  // Fill this in
+    let arr = [];
 
+    for (let i = increment; i <= increment * 10; i += increment) {
+        const res = addManyNums(i);
+        arr.push(res);
+    }
+
+    return arr;
 }
 
 module.exports = [addNums10, addManyNums10];
--- phase-3.js
+++ phase-3.js
@@ -1,29 +1,88 @@
 const [addNums, addManyNums] = require("./phase-1");
 
+// Define Big-O
+// "Number of operations an algorithm will make"
+// O(n)
 function addNums10Timing(increment) {
-  // Copy your `addNums10` code here
-  // Then, add timing code
+    // Then, add timing code
+    let arr = [];
 
-  // Your code here
+    for (let i = increment; i <= increment * 10; i += increment) {
+        const start = Date.now();
 
+        const res = addNums(i);
+        arr.push(res);
+
+        const end = Date.now();
+        console.log(end - start);
+    }
+
+    return arr;
 }
 
+// O(n)
+function addNums100Timing(increment) {
+    // Then, add timing code
+    let arr = [];
 
+    for (let i = increment; i <= increment * 100; i += increment) {
+        const start = Date.now();
+
+        const res = addNums(i);
+        arr.push(res);
+
+        const end = Date.now();
+        console.log(end - start);
+    }
+
+    return arr;
+}
+
+// O(n^2)
 function addManyNums10Timing(increment) {
-// Copy your `addManyNums10` code here
-// Then, add timing code
+    // Then, add timing code
+    let arr = [];
 
-  // Your code here
+    for (let i = increment; i <= increment * 10; i += increment) {
+        const start = Date.now();
 
+        const res = addManyNums(i);
+        arr.push(res);
+
+        const end = Date.now();
+        console.log(end - start);
+    }
+
+    return arr;
+}
+
+// O(n^2)
+function addManyNums100Timing(increment) {
+    // Then, add timing code
+    let arr = [];
+
+    for (let i = increment; i <= increment * 100; i += increment) {
+        const start = Date.now();
+
+        const res = addManyNums(i);
+        arr.push(res);
+
+        const end = Date.now();
+        console.log(end - start);
+    }
+
+    return arr;
 }
 
 
-n = 1000000
+n = 1000000;
 console.log(`addNums(${n}): `);
-addNums10Timing(1000000);
+addNums100Timing(1000000);
+
 
 console.log("\n***********\n");
 
-n = 1000
+
+n = 1000;
 console.log(`addManyNums(${n}): `);
-addManyNums10Timing(5000);
+addManyNums100Timing(250);
