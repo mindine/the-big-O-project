08485648e7593c893db1805b2b810fd62335b013
Adam Scoggins <scoggins.aj@gmail.com>
2022-08-16 23:03:44 -0500

    Added _addManyNums10Timing

--- phase-2.js
+++ phase-2.js
@@ -24,4 +24,43 @@ function addManyNums10(increment) {
     return arr;
 }
 
+
+
+
+function _addManyNums10(increment) {
+    let arr = [];
+
+    for (let i = increment; i <= increment * 10; i += increment) {
+        let sum = 0;
+        const memoIds = [];
+        const memoVals = [];
+
+        let curMemoId = memoIds[memoIds.length - 1];
+        if (curMemoId === undefined) {
+            curMemoId = i;
+        }
+
+        for (let j = curMemoId; j <= i; j++) {
+            const res = addNums(i);
+            memoIds.push(i);
+
+            const memoVal = memoVals[memoVals.length - 1];
+
+            if (memoVal !== undefined) {
+                sum += res + memoVal;
+            } else {
+                sum += res;
+            }
+
+            memoVals.push(res);
+        }
+
+        arr.push(sum);
+    }
+
+    return arr;
+}
+
+_addManyNums10(10);
+
 module.exports = [addNums10, addManyNums10];
--- phase-3.js
+++ phase-3.js
@@ -74,15 +74,62 @@ function addManyNums100Timing(increment) {
     return arr;
 }
 
+// More efficient
+// O(1)
+function _addManyNums10Timing(increment) {
+    let arr = [];
 
-n = 1000000;
-console.log(`addNums(${n}): `);
-addNums100Timing(1000000);
+    for (let i = increment; i <= increment * 10; i += increment) {
+        //const start = Date.now();
+
+        let sum = 0;
+        const memoIds = [];
+        const memoVals = [];
+
+        let curMemoId = memoIds[memoIds.length - 1];
+        if (curMemoId === undefined) {
+            curMemoId = i;
+        }
+
+        for (let j = curMemoId; j <= i; j++) {
+            const res = addNums(i);  
+            console.log(curMemoId, '=', res);
+            memoIds.push(i);
+
+            const memoVal = memoVals[memoVals.length - 1];
+
+            if (memoVal !== undefined) {
+                sum += res + memoVal;
+            } else {
+                sum += res;
+            }
+
+            memoVals.push(res);
+        }
+
+        arr.push(sum);
+
+        //const end = Date.now();
+        //console.log(end - start);
+    }
+
+    return arr;
+}
 
 
-console.log("\n***********\n");
+// n = 1000000;
+// console.log(`addNums(${n}): `);
+// addNums100Timing(1000000);
+
+
 
 
 n = 1000;
 console.log(`addManyNums(${n}): `);
-addManyNums100Timing(250);
+const res = _addManyNums10Timing(5000);
+console.log('Result:', res);
+
+// console.log("\n***********\n");
+// console.log(`addManyNums(${n}): `);
+// const res2 = addManyNums10Timing(5000);
+// console.log('Result:', res);
